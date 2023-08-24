8fd5be5958ef11e6b03c68c0fde92d41242f3b5a
DenisMac <denis.hellal@codeworks.fr>
2023-08-24 10:51:09 +0200

    Phase4: Ajout du service de recuperation des donnees

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,11 +8,26 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  hheroes: Hero[] = [];
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
 }
   
+
+
+getHeroes(): void {
+  this.hheroes = this.heroService.getHeroes();
+}
+
+
+ngOnInit(): void {
+  this.getHeroes();
+}
+
+constructor(private heroService: HeroService) {} // Permet de d'injecter le service 
+
+//Reserve the constructor for minimal initialization such as wiring constructor parameters to properties. The constructor shouldn't do anything. 
+
 }
