e4d0cb5be30ee38d51b8e6f420d0a3d259ba57c6
DenisMac <denis.hellal@codeworks.fr>
2023-08-26 21:03:06 +0200

    Phase6: ajout de la fonction de recherche

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -36,7 +36,7 @@ export class HeroesComponent implements OnInit {
   }
 
   delete(hero: Hero): void {
-    this.heroes = this.heroes.filter(h => h !== hero);
+    this.hheroes = this.hheroes.filter(h => h !== hero);
     this.heroService.deleteHero(hero.id).subscribe();
   }
 
